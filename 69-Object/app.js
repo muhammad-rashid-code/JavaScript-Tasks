ea07a708bb203637e1e4b55bdaa050cddf516658
Muhammad Rashid <m.rashid212823@gmail.com>
2024-05-27 15:17:14 -0700

    class 18 and 19 revided

---
+++ 69-Object/app.js
@@ -0,0 +1,38 @@
+// let plan1 = {
+//   planName: "fat loss",
+//   excercises: "strngth training",
+//   fee: 2000,
+//   admissionFee: 1000,
+//   isAirConditioned: false,
+//   calculateBill: function (noOfMonths) {
+//     let totalBill = this.fee * noOfMonths + this.admissionFee;
+//     if (noOfMonths >= 3) {
+//       totalBill = (totalBill / 100) * 80;
+//     }
+//     console.log(totalBill);
+//   },
+// };
+// // console.log(plan1.excercises);
+// // console.log(plan1["excercises"]);
+// // plan1.calculateBill(4);
+// // plan1.calculateBill(2);
+// // console.log(plan1);
+
+// // console.log(plan1["admissionFee"]);
+// // console.log(plan1["planName"]);
+
+// // let allKeys = Object.keys(plan1);
+// // let allValues = Object.values(plan1);
+// // console.log(allKeys, allValues);
+
+// for (let prop in plan1) {
+//   //   console.log(prop);
+//   //   console.log(plan1);
+//   //   console.log(plan1.planName);
+//   //   console.log(plan1.prop);
+//   console.log(prop);
+//   console.log(prop, ":", plan1[prop]);
+//   console.log(prop, ":", plan1[]);
+// }
+
+
