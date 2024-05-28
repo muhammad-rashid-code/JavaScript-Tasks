fe25b76352972002bd95b90228c9496586bffeda
Muhammad Rashid <m.rashid212823@gmail.com>
2024-05-28 06:10:27 -0700

    3-VARIABLES-FOR-NUMBERS/ probelems solved

---
+++ 3-VARIABLES-FOR-NUMBERS/app.js
@@ -0,0 +1,40 @@
+// alert("AsSalaamOalikum");
+
+// // 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+// let myAge;
+// myAge = 30;
+// alert("My age is " + myAge);
+
+// 2. Declare & initialize a variable to keep track of how manytimes a visitor has visited a web page. Show his/her number of
+//  visits on your web page. For example: “You have visited this site N times”.
+
+// let numOfVisit = 14;
+// alert("You have visit " + numOfVisit + " times");
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+// let myBirthYY = 1994;
+// document.write(
+//   "My birth Year is " +
+//     myBirthYY +
+//     "<br/>" +
+//     "The type of my variable is " +
+//     typeof myBirthYY
+// );
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// let viname = "Haider ali";
+// let proTitle = "T-Shirt(s)";
+// let num0fOrder = 15;
+
+// document.write(
+//   viname + " ordered " + num0fOrder + " " + proTitle + " on daraz"
+// );
