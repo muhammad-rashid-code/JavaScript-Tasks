f91cb2bb6bdffcb04f67c8887308af0f33c8a485
Muhammad Rashid <m.rashid212823@gmail.com>
2024-05-28 05:07:41 -0700

    js variable for string code problems solved

---
+++ 2-VARIABLES FOR STRINGS/app.js
@@ -0,0 +1,48 @@
+// 1. Declare a variable called username.
+// var userName;
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// userName = "Muhammad Rashid";
+// alert(userName);
+// alert("rashid");
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// var message;
+// message = "Hello World";
+// alert(message);
+
+// // 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var studentName = "Muhammad Rashid";
+// alert(studentName);
+
+// var studenAge = "30 Years";
+// alert(studenAge);
+
+// var occupation = "MERN stack Developer";
+// alert(occupation);
+
+// 5. Write a script to display the following alert using one JS variable:
+
+// var pizzaSlice = "pizza\npizz\npiz\npi\np";
+// alert(pizzaSlice);
+
+// var myEmail = "m.rashid212823@gmail.com";
+// var myEmailMsg = "My Email addres is ";
+// alert(myEmailMsg + myEmail);
+
+// 7. Declare a variable called book & give it the value “A
+// // smarter way to learn JavaScript”. Display the following message in an alert box:
+// var book;
+// book = "A samarter way to learn JavaScript";
+// alert("I am tryin to learn from The Book " + book);
+
+// 8. Write a script to display this in browser through JS
+// var msg = "Yah! I can write HTML content through javaScript";
+// document.write(msg);
+
+// 9. Store following string in a variable and show in alert and  browser through JS
+// var des = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(des);
