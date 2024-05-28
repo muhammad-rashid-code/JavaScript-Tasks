e78dd3ba2c83008f16965c9d8bcb329b677c5184
Muhammad Rashid <m.rashid212823@gmail.com>
2024-05-28 04:17:05 -0700

    alerts tasks added

---
+++ 1-alerts/app.js
@@ -0,0 +1,15 @@
+// // 1. Write a script to greet your website visitor using JS alert box.
+// window.alert("this is window.alert");
+// alert("This is only alert!");
+
+// 2. Write a script to display following message on your web page:
+// alert("Error! Please enter a valid password");
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break \n)
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+// 4. Write a script to display following messages in sequence:
+// alert("Wlecome to JS Land...");
+// alert("Happy Coding!");
+
+// 5. Generate the following message through browser’s developer console:
