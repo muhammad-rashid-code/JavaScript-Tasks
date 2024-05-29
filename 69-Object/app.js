5ff6b3e04bbf3425083b78a651a3cc431bc38979
Muhammad Rashid <m.rashid212823@gmail.com>
2024-05-29 15:26:11 -0700

    branch ka phadda

--- 69-Object/app.js
+++ 69-Object/app.js
@@ -158,3 +158,25 @@
 // let allKeys = Object.keys(armyPerson);
 // let allValues = Object.values(armyPerson);
 // console.log(allKeys + " : " + allValues);
+
+// boject creation
+// constructor function
+// for loop in funtion
+// getting values
+// getting keys
+// prototype
+// editing prototypy
+//
+
+let student1 = {
+  fName: "muhammad",
+  lName: "rashid",
+  rollNum: 42343,
+  subjects: ["HTML", "CSS", "JS"],
+  calculatePercentage: function (html, css, JS) {
+    let pre = ((html + css + JS) * 100) / 300;
+    console.log(this.calculatePercentage(93, 83, 73));
+  },
+};
+
+student1.calculatePercentage();
