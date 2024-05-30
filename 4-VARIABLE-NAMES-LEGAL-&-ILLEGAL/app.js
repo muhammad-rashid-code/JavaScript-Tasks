1dd6f626102415339217afcd56591bb13e580dbe
Muhammad Rashid <m.rashid212823@gmail.com>
2024-05-29 17:37:06 -0700

    js ch#4 practiced

---
+++ 4-VARIABLE-NAMES-LEGAL-&-ILLEGAL/app.js
@@ -0,0 +1,18 @@
+// 1. Declare 3 variables in one statement.
+let var1, var2, var3;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Legal variable ://////////////////
+// firstName
+// _age
+// myVar
+// camelCaseVariable
+// $price
+
+// Illegal variable names:////////////
+// 2ndName (Cannot start with a number)
+// my-variable (Cannot contain hyphens)
+// var (Reserved keyword)
+// my variable (Cannot contain spaces)
+// !illegal (Cannot contain special characters except for $ and _)
