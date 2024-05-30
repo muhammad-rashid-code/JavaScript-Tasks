471d4a97860b2a27fedaecf690ed0dc3a9e253bf
Muhammad Rashid <m.rashid212823@gmail.com>
2024-05-29 18:03:22 -0700

    math start

---
+++ 5-MATH-EXPRESSIONS/app.js
@@ -0,0 +1,28 @@
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+// let num1 = 3;
+// let num2 = 2;
+// alert(
+//   "The sum of number " + num1 + " and number " + num2 + " is " + (num1 + num2)+" ."
+// );
+
+// 2. Repeat task1 for subtraction, multiplication, division & modulus.
+ 
+// 3. Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+// b. Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+// c. Initialize the variable with some number.
+// d. Show the value of variable in your browser like “Initial
+// value: 5”.
+// e. Increment the variable.
+// f. Show the value of variable in your browser like “Value
+// after increment is: 6”.
+// g. Add 7 to the variable.
+// h. Show the value of variable in your browser like “Value after addition is: 13”.
+// i. Decrement the variable.
+// j. Show the value of variable in your browser like “Value
+// after decrement is: 12”.
+// k. Show the remainder after dividing the variable’s value
+// by 3.
+// l. Output : “The remainder is : 0”.
+
